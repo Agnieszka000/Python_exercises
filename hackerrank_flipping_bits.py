35c1bfec4dc26f743619451f707184f3001b8ef0
Agnieszka000 <dluska.agnieszka@gmail.com>
2025-01-08 09:22:02 +0100

    HackerRank Flipping Bits challenge added

---
+++ hackerrank_flipping_bits.py
@@ -0,0 +1,41 @@
+# HackerRank Challenge: Flipping bits
+#
+# You will be given a list of 32 bit unsigned integers. Flip all the bits ( and ) and return the result as an unsigned integer. 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+def flippingBits(n):
+    # Write your code here
+    # Flip the bits using bitwise NOT (~)
+    flipped = ~n
+    # Mask the flipped result to ensure it's treated as an unsigned 32-bit integer
+    # We need it because Python treats binaries as signed numbers
+    # # Masking to 32 bits in binary
+    flipped_unsigned = flipped & 0b11111111111111111111111111111111
+    return flipped_unsigned
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + '\n')
+
