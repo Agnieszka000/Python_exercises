0e290bd2948d353efdc65dd5d5011d909a1cf0d5
Agnieszka000 <dluska.agnieszka@gmail.com>
2025-03-18 18:04:59 +0100

    Hackerrank Sales by Match added

---
+++ hackerrank_sales_by_match.py
@@ -0,0 +1,26 @@
+# Hackerrank.com - Challenge: "Sales by match"
+
+# There is a large pile of socks that must be paired by color. 
+# Given an array of integers representing the color of each sock, 
+# determine how many pairs of socks with matching colors there are.
+
+# number of socks
+n = 7
+# each digit represents a colour
+ar = [1,2,1,2,1,3,2] 
+
+
+def sockMerchant(n, ar):
+    
+    # Use set to find how many unique colours there are in ar:
+    unique_colours = set(ar)
+    
+    # Check how many socks of a unique colour there are:
+    for colour in unique_colours:
+        # Use .count to count all the items of the same colour:
+        same_colour_count = ar.count(colour)
+        pairs = same_colour_count // 2 
+        print(f"The colour {colour} appears {same_colour_count} times.")
+        print(f"There is/are {pairs} pair(s) of socks in the colour {colour}.")
+    
+sockMerchant(n,ar)
